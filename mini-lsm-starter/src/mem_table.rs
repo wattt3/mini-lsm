54dafa630ef060e64b33a49e68560b52df7acd5c
wattt3 <chleodnr3@gmail.com>
2025-09-02 21:45:47 +0900

    implement MemTable creation and basic get/put functionality

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -17,6 +17,7 @@
 
 use std::ops::Bound;
 use std::path::Path;
+use std::ptr::null;
 use std::sync::Arc;
 use std::sync::atomic::AtomicUsize;
 
@@ -53,7 +54,12 @@ pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
 impl MemTable {
     /// Create a new mem-table.
     pub fn create(_id: usize) -> Self {
-        unimplemented!()
+        Self {
+            id: _id,
+            map: Arc::new(SkipMap::new()),
+            wal: None,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        }
     }
 
     /// Create a new mem-table with WAL
@@ -87,7 +93,7 @@ impl MemTable {
 
     /// Get a value by key.
     pub fn get(&self, _key: &[u8]) -> Option<Bytes> {
-        unimplemented!()
+        self.map.get(_key).map(|x| x.value().clone())
     }
 
     /// Put a key-value pair into the mem-table.
@@ -96,7 +102,8 @@ impl MemTable {
     /// In week 2, day 6, also flush the data to WAL.
     /// In week 3, day 5, modify the function to use the batch API.
     pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+        self.map.insert(Bytes::copy_from_slice(_key), Bytes::copy_from_slice(_value));
+        Ok(())
     }
 
     /// Implement this in week 3, day 5; if you want to implement this earlier, use `&[u8]` as the key type.
